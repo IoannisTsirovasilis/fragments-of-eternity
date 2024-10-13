0d677826261dcf13bd5c900c76d0ecdb6311318e
Ioannis Tsirovasilis <ioannistsirovasilis@gmail.com>
2024-10-13 22:40:14 +0300

    Add social media icons

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,28 @@
 import Link from "next/link";
+import InstagramIcon from "./common/Instagram";
+import TiktokIcon from "./common/Tiktok";
 
 export default function footer() {
   return (
-    <footer className="py-8 bg-gray-900 text-gray-400 text-center">
+    <footer className="flex flex-col gap-2 py-8 bg-gray-900 text-gray-400 text-center">
       <p>
         Centuries Echoing Legends Eventually Shape The Ancient Ruins Around.
+      </p>
+      <p className="flex items-center justify-center gap-4">
+        <Link
+          href="https://www.instagram.com/fragments_of_eternity_/"
+          className="rounded-full transition ease-in-out duration-200"
+          target="_blank"
+        >
+          <InstagramIcon />
+        </Link>
+        <Link
+          href="https://www.tiktok.com/@fragments_of_eternity_"
+          className="rounded-full transition ease-in-out duration-200"
+          target="_blank"
+        >
+          <TiktokIcon />
+        </Link>
       </p>
       <p>
         <Link
